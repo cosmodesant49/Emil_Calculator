f1ff209cdb5c8125b9da3d28cd7aa03432eef612
cosmodesant49 <emilmaldybaev49@gmail.com>
2023-09-28 18:05:45 +0600

    v1

--- app/src/main/java/com/geeks/emil_calculator/MainActivity.java
+++ app/src/main/java/com/geeks/emil_calculator/MainActivity.java
@@ -1,16 +1,18 @@
 package com.geeks.emil_calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private TextView textView;
     private Integer first, second;
-    private String operator;
     private Boolean isOperationClick;
+    private String operation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +21,16 @@ public class MainActivity extends AppCompatActivity {
         textView = findViewById(R.id.text_view);
     }
 
+    private void testText(String t){
+
+    }
+
     public void onNumberClick(View view) {
+        //проверка на показ числа с кнопки и склейка
         if (view.getId() == R.id.btn_clear) {
             textView.setText("0");
-            first = null;
-            second = null;
-            operator = null;
+            first = 0;
+            second = 0;
         } else {
             String textButton = ((Button) view).getText().toString();
             if (textView.getText().toString().equals("0") || isOperationClick) {
@@ -37,45 +43,43 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onOperationClick(View view) {
-        if (view.getId() == R.id.btn_equal) {
-            if (operator != null) {
-                second = Integer.valueOf(textView.getText().toString());
-                performOperation();
-            }
-        } else {
-            if (first == null) {
-                first = Integer.valueOf(textView.getText().toString());
-            } else {
-                second = Integer.valueOf(textView.getText().toString());
-                performOperation();
-            }
-            operator = ((Button) view).getText().toString();
+        if (view.getId() == R.id.btn_plus) {
+            first = Integer.valueOf(textView.getText().toString());
             isOperationClick = true;
-        }
-    }
+            operation = "+";
+        } else if (view.getId() == R.id.btn_minus) {
+            first = Integer.valueOf(textView.getText().toString());
+            isOperationClick = true;
+            operation = "-";
+        } else if (view.getId() == R.id.btn_multiply) {
+            first = Integer.valueOf(textView.getText().toString());
+            isOperationClick = true;
+            operation = "*";
+        } else if (view.getId() == R.id.btn_divide) {
+            first = Integer.valueOf(textView.getText().toString());
+            isOperationClick = true;
+            operation = "/";
+        } else if (view.getId() == R.id.btn_equal) {
+            second = Integer.valueOf(textView.getText().toString());
+            Integer result = 0;
 
-    private void performOperation() {
-        if (operator != null && second != null) {
-            int result = 0;
-            switch (operator) {
-                case "+":
-                    result = first + second;
-                    break;
-                case "-":
-                    result = first - second;
-                    break;
-                case "*":
-                    result = first * second;
-                    break;
-                case "/":
-                    if (second != 0) {
-                        result = first / second;
-                    }
-                    break;
+            if (operation.equals("+")) {
+                result = first + second;
+            } else if (operation.equals("-")) {
+                result = first - second;
+            } else if (operation.equals("*")) {
+                result = first * second;
+            } else if (operation.equals("/")) {
+                if (second != 0) {
+                    result = first / second;
+                } else {
+                    Toast.makeText(this, "Ошибка: Деление на ноль", Toast.LENGTH_SHORT).show();
+                    return;
+                }
             }
-            textView.setText(String.valueOf(result));
-            first = result;
-            second = null;
+
+            textView.setText(result.toString());
+            isOperationClick = false;
         }
     }
 }
